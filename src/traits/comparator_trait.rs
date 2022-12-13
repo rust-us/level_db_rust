c311c1741fb3bb3e7d729e8aa9b9b51a3ec34b5f
fengyang <yueny09@163.com>
2022-12-13 18:45:25 +0800

    ComparatorTrait 定义

--- src/traits/comparator_trait.rs
+++ src/traits/comparator_trait.rs
@@ -12,8 +12,7 @@ pub trait ComparatorTrait {
     /// 返回comparator的名字
     fn get_name() -> String;
 
-    /// 减少像index blocks这样的内部数据结构占用的空间
-    /// 这两个函数作用是减少像index blocks这样的内部数据结构占用的空间。
+    /// 找到start、limit之间最短的字符串，如“helloworld”和”hellozoomer”之间最短的key可以是”hellox”。
     /// 如果 start < limit，就在[start,limit)中找到一个短字符串，并赋给*start返回。
     /// 当然返回的*start可能没变化（start==limit），此时这个函数相当于啥都没干，这也是正确的。
     fn find_shortest_separator(&self, start: &String, limit:&Slice) -> String;
