5e7ae5933e48f248fca20f21beba6a31b571033c
fengyang <yueny09@163.com>
2023-01-10 21:16:31 +0800

    Status 逻辑精简

--- src/util/filter_policy.rs
+++ src/util/filter_policy.rs
@@ -57,12 +57,20 @@ impl FilterPolicy for BloomFilterPolicy {
         String::from("leveldb.BuiltinBloomFilter2")
     }
 
-    fn create_filter(&self, keys: Slice, n: u32, dst: String) -> String {
+    fn create_filter(&self, keys: Slice, n: usize) -> String {
         // 根据指定的参数创建过滤器，并返回结果， 结果为dst的原始内容 + append结果。
-        // 参数keys[0,n-1]包含依据用户提供的comparator排序的key列表--可重复，
-        // 并把根据这些key创建的filter追加到 dst中。
-        //
-        todo!()
+        // 参数keys[0,n-1]包含依据用户提供的comparator排序的key列表--可重复， 并把根据这些key创建的filter追加到 dst中。
+        let mut bits: usize = n * self.bits_per_key;
+
+        // For small n, we can see a very high false positive rate.  Fix it
+        // by enforcing a minimum bloom filter length.
+        if bits < 64 {
+            bits = 64;
+        }
+
+        let bytes: usize = (bits + 7) / 8;
+        bits = bytes * 8;
+
     }
 
     fn key_may_match(key: &Slice, filter: &Slice) -> bool {
