11f4c7c73b42cb6689b65816ba61ce8e1e20b71f
fengyang <yueny09@163.com>
2023-01-17 18:39:25 +0800

    FileMetaData 实现

--- src/db/file_meta_data.rs
+++ src/db/file_meta_data.rs
@@ -1,5 +1,6 @@
 use crate::db::db_format::InternalKey;
 
+/// @see version_edit FileMetaData
 pub struct FileMetaData {
     refs: u32,
     // Seeks allowed until compaction
@@ -7,6 +8,22 @@ pub struct FileMetaData {
     number: u64,
     // File size in bytes
     file_size: u64,
+    // Smallest internal key served by table
     smallest: InternalKey,
+    // Largest internal key served by table
+    largest: InternalKey
+}
 
+impl Default for FileMetaData {
+    #[inline]
+    fn default() -> Self {
+        Self {
+            refs: 0,
+            allowed_seeks: 1 << 30,
+            number: 0,
+            file_size: 0,
+            smallest: InternalKey::default(),
+            largest: InternalKey::default()
+        }
+    }
 }
